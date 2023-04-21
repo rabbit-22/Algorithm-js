7ad78a3de30643435f46f071da84b4daf16f64fc
rabbit-22 <chlwlal20@gmail.com>
2023-04-22 01:49:36 +0900

    4/21

---
+++ 프로그래머스/완주하지 못한 선수.js
@@ -0,0 +1,9 @@
+function solution(participant, completion) {
+  const arr1 = participant.sort();
+  const arr2 = completion.sort();
+  for (let i = 0; i <= arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return arr1[i];
+    }
+  }
+}
