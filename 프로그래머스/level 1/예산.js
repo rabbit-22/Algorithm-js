563b377018c32cb922e913875f2b0a5e9058e4a7
rabbit-22 <chlwlal20@gmail.com>
2023-07-29 22:57:21 +0900

    7/29

---
+++ 프로그래머스/level 1/예산.js
@@ -0,0 +1,10 @@
+function solution(d, budget) {
+  let sum = 0,
+    count = 0;
+  d.sort((a, b) => a - b);
+  for (let i = 0; i < d.length; i++) {
+    sum += d[i];
+    if (sum <= budget) count++;
+  }
+  return count;
+}
